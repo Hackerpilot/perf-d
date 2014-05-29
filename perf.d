6c90afcf49f584ab5e6030ab1a4c5827f32a1235
Hackerpilot <briancschott@gmail.com>
2014-05-28 18:16:34 -0700

    More structs

--- perf.d
+++ perf.d
@@ -1,5 +1,7 @@
 module perf;
 
+alias pid_t = int;
+
 /**
  * Magic number for version 1 of the Perf file format
  */
@@ -29,6 +31,42 @@ enum ulong PERF_HEADER_VERSION_2 =
 enum PERF_EVLIST__HLIST_BITS = 8;
 enum PERF_EVLIST__HLIST_SIZE = (1 << PERF_EVLIST__HLIST_BITS);
 
+enum
+{
+	PERF_SAMPLE_IP				= 1U << 0,
+	PERF_SAMPLE_TID				= 1U << 1,
+	PERF_SAMPLE_TIME			= 1U << 2,
+	PERF_SAMPLE_ADDR			= 1U << 3,
+	PERF_SAMPLE_READ			= 1U << 4,
+	PERF_SAMPLE_CALLCHAIN			= 1U << 5,
+	PERF_SAMPLE_ID				= 1U << 6,
+	PERF_SAMPLE_CPU				= 1U << 7,
+	PERF_SAMPLE_PERIOD			= 1U << 8,
+	PERF_SAMPLE_STREAM_ID			= 1U << 9,
+	PERF_SAMPLE_RAW				= 1U << 10,
+	PERF_SAMPLE_BRANCH_STACK		= 1U << 11,
+	PERF_SAMPLE_REGS_USER			= 1U << 12,
+	PERF_SAMPLE_STACK_USER			= 1U << 13,
+	PERF_SAMPLE_WEIGHT			= 1U << 14,
+	PERF_SAMPLE_DATA_SRC			= 1U << 15,
+	PERF_SAMPLE_IDENTIFIER			= 1U << 16,
+	PERF_SAMPLE_TRANSACTION			= 1U << 17,
+	PERF_SAMPLE_MAX = 1U << 18,		/* non-ABI */
+}
+
+enum PERF_SAMPLE_MASK =
+	  PERF_SAMPLE_IP
+	| PERF_SAMPLE_TID
+	| PERF_SAMPLE_TIME
+	| PERF_SAMPLE_ADDR
+	| PERF_SAMPLE_ID
+	| PERF_SAMPLE_STREAM_ID
+	| PERF_SAMPLE_CPU
+	| PERF_SAMPLE_PERIOD
+	| PERF_SAMPLE_IDENTIFIER;
+
+enum PERF_SAMPLE_MAX_SIZE = 1 << 16;
+
 enum {
 	HEADER_RESERVED		= 0,	/* always cleared */
 	HEADER_FIRST_FEATURE	= 1,
@@ -54,12 +92,33 @@ enum {
 	HEADER_FEAT_BITS	= 256,
 }
 
-enum PERF_SAMPLE_MAX_SIZE = 1 << 16;
+struct EventHeader {
+	uint type;
+	ushort misc;
+	ushort size;
+}
 
 enum HeaderVersion {
 	PERF_HEADER_VERSION_1,
 	PERF_HEADER_VERSION_2,
 }
+
+enum
+{
+	PERF_RECORD_MMAP			= 1,
+	PERF_RECORD_LOST			= 2,
+	PERF_RECORD_COMM			= 3,
+	PERF_RECORD_EXIT			= 4,
+	PERF_RECORD_THROTTLE			= 5,
+	PERF_RECORD_UNTHROTTLE			= 6,
+	PERF_RECORD_FORK			= 7,
+	PERF_RECORD_READ			= 8,
+	PERF_RECORD_SAMPLE			= 9,
+	PERF_RECORD_MMAP2			= 10,
+	PERF_RECORD_MAX,			/* non-ABI */
+}
+
+enum PATH_MAX = 4096;
 
 struct FileSection {
 	ulong offset;
@@ -86,11 +145,247 @@ struct Header {
 	SessionEnv env;
 }
 
-//struct TraceEvent
-//{
-//	PEvent* pevent;
-//	PluginList* plugin_list;
-//}
+union Event {
+	EventHeader	header;
+	MMapEvent mmap;
+	MMap2Event mmap2;
+	CommEvent comm;
+	ForkEvent fork;
+	LostEvent lost;
+	ReadEvent read;
+	ThrottleEvent throttle;
+	SampleEvent sample;
+	AttrEvent attr;
+	EventTypeEvent event_type;
+	TracingDataEvent tracing_data;
+	BuildIdEvent build_id;
+}
+
+struct SampleEvent {
+	EventHeader header;
+	ubyte* array;
+}
+
+struct TracingDataEvent {
+	EventHeader header;
+	uint size;
+}
+
+struct EventTypeEvent {
+	EventHeader header;
+	TraceEventType event_type;
+}
+
+struct EventAttr {
+	uint			type;
+	uint			size;
+	ulong			config;
+	union {
+		ulong		sample_period;
+		ulong		sample_freq;
+	}
+	ulong			sample_type;
+	ulong			read_format;
+	import std.bitmanip;
+	mixin(bitfields!(
+		ulong, "disabled", 1,
+		ulong, "inherit", 1,
+		ulong, "pinned", 1,
+		ulong, "exclusive", 1,
+		ulong, "exclude_user", 1,
+		ulong, "exclude_kernel", 1,
+		ulong, "exclude_hv", 1,
+		ulong, "exclude_idle", 1,
+		ulong, "mmap", 1,
+		ulong, "comm", 1,
+		ulong, "freq", 1,
+		ulong, "inherit_stat", 1,
+		ulong, "enable_on_exec" , 1,
+		ulong, "task", 1,
+		ulong, "watermark", 1,
+		ulong, "precise_ip", 2,
+		ulong, "mmap_data", 1,
+		ulong, "sample_id_all", 1,
+		ulong, "exclude_host", 1,
+		ulong, "exclude_guest", 1,
+		ulong, "exclude_callchain_kernel", 1,
+		ulong, "exclude_callchain_user", 1,
+		ulong, "mmap2",  1,
+		ulong, "__reserved_1", 40
+	));
+	union {
+		uint		wakeup_events;
+		uint		wakeup_watermark;
+	}
+	uint			bp_type;
+	union {
+		ulong		bp_addr;
+		ulong		config1;
+	}
+	union {
+		ulong		bp_len;
+		ulong		config2;
+	}
+	ulong	branch_sample_type;
+	ulong	sample_regs_user;
+	uint	sample_stack_user;
+	uint	__reserved_2;
+}
+
+struct AttrEvent {
+	EventHeader header;
+	EventAttr attr;
+	ulong* id;
+}
+
+struct MMapEvent {
+	EventHeader header;
+	uint pid;
+	uint tid;
+	ulong start;
+	ulong len;
+	ulong pgoff;
+	char[PATH_MAX] filename;
+}
+
+struct MMap2Event {
+	EventHeader header;
+	uint pid;
+	uint tid;
+	ulong start;
+	ulong len;
+	ulong pgoff;
+	uint maj;
+	uint min;
+	ulong ino;
+	ulong ino_generation;
+	char[PATH_MAX] filename;
+}
+
+struct CommEvent {
+	EventHeader header;
+	uint pid;
+	uint tid;
+	char[16] comm;
+}
+
+struct ForkEvent {
+	EventHeader header;
+	uint pid;
+	uint ppid;
+	uint tid;
+	uint ptid;
+	ulong time;
+}
+
+struct LostEvent {
+	EventHeader header;
+	ulong id;
+	ulong lost;
+}
+
+struct ReadEvent {
+	EventHeader header;
+	uint pid;
+	uint tid;
+	ulong value;
+	ulong time_enabled;
+	ulong time_running;
+	ulong id;
+}
+
+struct ThrottleEvent {
+	EventHeader header;
+	ulong time;
+	ulong id;
+	ulong stream_id;
+}
+
+struct SampleReadValue {
+	ulong value;
+	ulong id;
+}
+
+struct SampleRead {
+	ulong time_enabled;
+	ulong time_running;
+	union {
+		struct Group {
+			ulong nr;
+			SampleReadValue* values;
+		}
+		SampleReadValue one;
+	}
+}
+
+enum BUILD_ID_SIZE = 20;
+size_t PERF_ALIGN(size_t x, size_t a) { return (x + (a - 1)) & (~(a - 1)); }
+
+struct BuildIdEvent {
+	EventHeader header;
+	pid_t pid;
+	ubyte[PERF_ALIGN(BUILD_ID_SIZE, ulong.sizeof)] build_id;
+	char* filename;
+}
+
+struct RegsDump {
+	ulong abi;
+	ulong* regs;
+}
+
+struct IPCallchain {
+	ulong nr;
+	ulong[0] ips;
+}
+
+struct BranchStack {
+	ulong nr;
+	BranchEntry[0] entries;
+}
+
+struct BranchFlags {
+	import std.bitmanip;
+	mixin(bitfields!(
+		ulong, "mispred", 1,
+		ulong, "predicted", 1,
+		ulong, "in_tx", 1,
+		ulong, "abort", 1,
+		ulong, "reserved", 60
+	));
+}
+
+struct BranchEntry {
+	ulong from;
+	ulong to;
+	BranchFlags flags;
+}
+
+struct StackDump {
+	ushort offset;
+	ulong size;
+	char *data;
+}
+
+struct Sample {
+	ulong ip;
+	uint pid, tid;
+	ulong time;
+	ulong addr;
+	ulong id;
+	ulong stream_id;
+	ulong period;
+	ulong weight;
+	ulong transaction;
+	uint cpu;
+	uint raw_size;
+	ulong data_src;
+	void* raw_data;
+	IPCallchain* callchain;
+	BranchStack* branch_stack;
+	RegsDump  user_regs;
+	StackDump user_stack;
+	SampleRead read;
+}
 
 struct TraceEventType {
 	ulong event_id;
@@ -144,32 +439,6 @@ struct DataFile
 	ulong size;
 	DataMode mode;
 }
-
-//struct Evlist
-//{
-//	ListHead entries;
-//	HListHead[PERF_EVLIST__HLIST_SIZE] heads;
-//	int nr_entries;
-//	int nr_groups;
-//	int nr_fds;
-//	int nr_mmaps;
-//	size_t mmap_len;
-//	int id_pos;
-//	int is_pos;
-//	ulong combined_sample_type;
-//	struct Workload
-//	{
-//		int	cork_fd;
-//		pid_t	pid;
-//	}
-//	bool		 overwrite;
-//	MMap* mmap;
-//	Pollfd* pollfd;
-//	ThreadMap* threads;
-//	CPUMap* cpus;
-//	Evsel* selected;
-//}
-
 
 struct EvselStrHandler
 {
