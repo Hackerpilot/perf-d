f22d0cb6f3e6ec32262af73d3d5adcbca767172a
Hackerpilot <briancschott@gmail.com>
2014-05-29 17:18:51 -0700

    More progress

--- perftest.d
+++ perftest.d
@@ -2,12 +2,6 @@ import perf;
 
 import std.stdio;
 
-union headermagic
-{
-	ubyte[8] bytes;
-	ulong number;
-}
-
 void main(string[] args)
 {
 	FileHeader header;
@@ -18,6 +12,46 @@ void main(string[] args)
 	writeln("Header attr size: ", header.attr_size);
 	writeln("attrs.offset: ", header.attrs.offset);
 	writeln("attrs.size: ", header.attrs.size);
+	writeln("data.offset: ", header.data.offset);
+	writeln("data.size: ", header.data.size);
 	writeln("header.event_types.offset: ", header.event_types.offset);
 	writeln("header.event_types.size: ", header.event_types.size);
+//	return;
+
+	size_t nextOffset = header.data.offset;
+	uint[PERF_RECORD_MAX] counts;
+	foreach (i; 0 .. header.data.size)
+	{
+		f.seek(nextOffset);
+		EventHeader e;
+		f.rawRead((cast(ubyte*) &e)[0 .. EventHeader.sizeof]);
+		switch (e.type)
+		{
+		case PERF_RECORD_MMAP:
+		case PERF_RECORD_LOST:
+		case PERF_RECORD_COMM:
+		case PERF_RECORD_EXIT:
+		case PERF_RECORD_THROTTLE:
+		case PERF_RECORD_UNTHROTTLE:
+		case PERF_RECORD_FORK:
+		case PERF_RECORD_READ:
+		case PERF_RECORD_SAMPLE:
+		case PERF_RECORD_MMAP2:
+		case PERF_RECORD_MAX:
+			counts[e.type - 1]++;
+		default:
+			break;
+		}
+		nextOffset += e.size;
+	}
+	writeln("mmap: ", counts[PERF_RECORD_MMAP - 1]);
+	writeln("lost: ", counts[PERF_RECORD_LOST - 1]);
+	writeln("comm: ", counts[PERF_RECORD_COMM - 1]);
+	writeln("exit: ", counts[PERF_RECORD_EXIT - 1]);
+	writeln("throttle: ", counts[PERF_RECORD_THROTTLE - 1]);
+	writeln("unthrottle: ", counts[PERF_RECORD_UNTHROTTLE - 1]);
+	writeln("fork: ", counts[PERF_RECORD_FORK - 1]);
+	writeln("read: ", counts[PERF_RECORD_READ - 1]);
+	writeln("sample: ", counts[PERF_RECORD_SAMPLE - 1]);
+	writeln("mmap2: ", counts[PERF_RECORD_MMAP2 - 1]);
 }
